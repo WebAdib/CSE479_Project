0d694686af13449aac8dc573fcec9c69fa423658
webadib <webadib@gmail.com>
2024-05-01 18:28:14 +0600

    Section 2

--- index.php
+++ index.php
@@ -7,16 +7,26 @@
     <title>CSE479 Project</title>
 
     <!-- bootstrap cdn link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- css file -->
     <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="East West University_files/lightbox.min.css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/animate.css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/aos.css" id="bad5"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/owl.theme.default.min.css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/owl.carousel.min.css">
+    <link rel="stylesheet" href="East West University_files/fonts.css">
+    <link rel="stylesheet" href="East West University_files/slick.css">
+    <link rel="stylesheet" href="East West University_files/magnific-popup.css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/pb.calendar.css" type="text/css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/style-core.css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/kingster-style-custom.css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/style.css"> -->
+    <!-- <link rel="stylesheet" href="East West University_files/responsive.css"> -->
 </head>
 
 <body>
@@ -28,9 +38,7 @@
                 <!-- Icon -->
                 <a href="index.html"><img class="logoImage" src="img/logo02.png" alt=""></a>
                 <!-- navigation toggle button -->
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <!-- <span class="navbar-toggler-icon"></span> -->
                     <i class="fa-solid fa-bars"></i>
                 </button>
@@ -41,14 +49,12 @@
                             <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#"
-                                data-bs-auto-close="outside">Faculty O.H</a>
+                            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" data-bs-auto-close="outside">Faculty O.H</a>
                             <ul class="dropdown-menu shadow">
                                 <li><a class="dropdown-item" href="#">Faculty of Businesses & Economy</a></li>
                                 <li><a class="dropdown-item" href="#">Faculty of Liberal Arts & Social Science</a></li>
                                 <li class="dropend">
-                                    <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown"
-                                        data-bs-auto-close="outside" href="#">Faculty of Science & Engineering</a>
+                                    <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" href="#">Faculty of Science & Engineering</a>
                                     <ul class="dropdown-menu shadow">
                                         <li><a class="dropdown-item" href="#">Faculty of Electrical and Electronic
                                                 Engineering</a></li>
@@ -59,8 +65,7 @@
                                                 Sciences</a></li>
                                         <li><a class="dropdown-item" href="#">Faculty of Pharmacy</a></li>
                                         <li class="dropend">
-                                            <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown"
-                                                data-bs-auto-close="outside" href="#">Faculty of Computer Science &
+                                            <a class="dropdown-item dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" href="#">Faculty of Computer Science &
                                                 Engineering</a>
                                             <ul class="dropdown-menu dropdown-submenu shadow" id="list">
                                                 <button class="btnSort" onclick="sortList()">Sort List</button>
@@ -124,8 +129,7 @@
                         </li>
                         <!-- mega menu  -->
                         <li class="nav-item dropdown dropdown-mega position-static">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" id="navbarDropdown"
-                                data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
                                 Club Updates
                             </a>
                             <div class="dropdown-menu shadow">
@@ -190,8 +194,7 @@
                     </ul>
                     <form class="d-flex ms-auto" role="search">
                         <div class="input-group">
-                            <input class="form-control border-2 me-0 search" type="search" placeholder="Search"
-                                aria-label="Search">
+                            <input class="form-control border-2 me-0 search" type="search" placeholder="Search" aria-label="Search">
                             <button class="btn btn-outline-success" type="submit">Search</button>
                         </div>
                     </form>
@@ -200,16 +203,12 @@
         </nav>
     </header>
 
-    <!-- <section class="my-5"></section> -->
     <!-- carousel section  -->
     <div id="carouselExampleDark" class="carousel carousel-dark slide">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
-                aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-                aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-                aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="10000">
@@ -260,6 +259,51 @@
         </button>
     </div>
 
+    <!-- Section 2 section start  -->
+    <div class="section">
+        <div class="services">
+            <div class="card">
+                <div class="icon">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                </div>
+                <h2>Degree Program</h2>
+                <p>Join world's brightest minds</p>
+                <a href="" class="btn-se2-b1">Read More</a>
+            </div>
+
+            <div class="card">
+                <div class="icon">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                </div>
+                <h2>Student's Welfare</h2>
+                <p>Depertment of student welfare</p>
+                <a href="" class="btn-se2-b1">Read More</a>
+            </div>
+
+            <div class="card">
+                <div class="icon">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                </div>
+                <h2>Scholership</h2>
+                <p>Scholership & financial Aid</p>
+                <a href="" class="btn-se2-b1">Read More</a>
+            </div>
+
+            <div class="card">
+                <div class="icon">
+                    <i class="fa-solid fa-graduation-cap"></i>
+                </div>
+                <h2>Alumni</h2>
+                <p>Our Alumni is our Strength</p>
+                <a href="" class="btn-se2-b1">Read More</a>
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Section 2 end  -->
+
     <!-- footer section start  -->
     <footer>
         <div class="container">
@@ -268,9 +312,7 @@
                 <p> <i class="fa-solid fa-location-dot"></i> A/2, Jahurul Islam Avenue
                     Jahurul Islam City, Aftabnagar
                     Dhaka-1212, Bangladesh</p>
-                <p> <i class="fa-solid fa-phone"></i> <a href="tel:02-55046678">02-55046678</a>, <a
-                        href="tel:09666775577">09666775577</a>,<a href="tel:01755587224">01755587224</a>, <a
-                        href="tel:01851933094">01851933094</a></p>
+                <p> <i class="fa-solid fa-phone"></i> <a href="tel:02-55046678">02-55046678</a>, <a href="tel:09666775577">09666775577</a>,<a href="tel:01755587224">01755587224</a>, <a href="tel:01851933094">01851933094</a></p>
                 <p><i class="fa-solid fa-envelope"> </i><a href="mailto:admissions@ewubd.edu"> admissions@ewubd.edu</a>
                     <a href="mailto:info@ewubd.edu">info@ewubd.edu</a>
                 </p>
@@ -302,10 +344,7 @@
             </div>
             <div class="map">
                 <h2>Location</h2>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14605.625308688122!2d90.41520595550536!3d23.768540967336648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7892dcf0001%3A0x853ad729be4edc71!2z4KaH4Ka44KeN4KafIOCmk-Cmr-CmvOCnh-CmuOCnjeCmnyDgpofgpongpqjgpr_gpq3gpr7gprDgp43gprjgpr_gpp_gpr8!5e0!3m2!1sbn!2sbd!4v1713638195002!5m2!1sbn!2sbd"
-                    width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"
-                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14605.625308688122!2d90.41520595550536!3d23.768540967336648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7892dcf0001%3A0x853ad729be4edc71!2z4KaH4Ka44KeN4KafIOCmk-Cmr-CmvOCnh-CmuOCnjeCmnyDgpofgpongpqjgpr_gpq3gpr7gprDgp43gprjgpr_gpp_gpr8!5e0!3m2!1sbn!2sbd!4v1713638195002!5m2!1sbn!2sbd" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </footer>
@@ -318,9 +357,7 @@
 
 
     <!-- bootstrap JS link -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <!-- other js file link  -->
     <script src="js/sort.js"></script>
